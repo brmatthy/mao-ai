2f3304e0041b9c251379eded67f81515d853ac38
MatsVanBelle <mats.vanbelle.2002@gmail.com>
2022-10-18 09:33:11 +0200

    Added simple simulation to main

--- main.cpp
+++ main.cpp
@@ -6,13 +6,30 @@
 
 #include <iostream>
 #include "ai/LayCardSimulator.h"
-#include "ai/LayCardAi.h"
+#include "util/GeneticAi.h"
 #include "util/GeneticAlgorithm.h"
 #include "util/NeuralNetwork.h"
 
 int main(){
 
-    std::cout << "Hello world";
-    NeuralNetwork* network = new NeuralNetwork(52, 10, 5, 52);
+    std::cout << "Starting simulation" << std::endl;
+    int size = 100;
+    auto* network = new NeuralNetwork(52, 10, 5, 52);
+    auto** ais = new GeneticAi*[size];
+    for(int i = 0; i < size; i++)
+    {
+        ais[i] = new GeneticAi(52*10+10*10+52*10);
+    }
+    LayCardSimulator* simulator = new LayCardSimulator(network);
+    GeneticAlgorithm* algorithm = new GeneticAlgorithm(ais, size, simulator);
+    algorithm->simulate(100);
+    delete algorithm;
+    delete simulator;
+    for(int i = 0; i < size; i++)
+    {
+        delete ais[i];
+    }
+    delete[] ais;
     delete network;
+    std::cout << "Done!" << std::endl;
 }
