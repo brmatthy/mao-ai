52a8cf5243097f5e2d2393ce89ef658748af1090
brent <brent.matthys@Ugent.be>
2022-10-20 18:35:00 +0200

    create actAi

---
+++ ai/ActAi/QActAI.h
@@ -0,0 +1,22 @@
+//
+// Created by brent on 20/10/22.
+//
+
+#ifndef MAO_AI_QACTAI_H
+#define MAO_AI_QACTAI_H
+
+
+#include "../../player/interfaces/ActInterface.h"
+
+class QActAI : public ActInterface {
+public:
+    const std::unordered_multiset<Act> act(const std::deque<Action> &played, const ImmutableCard *played_card) override;
+
+    void acceptCorrection(const Correction &correction) override;
+};
+
+
+
+
+
+#endif //MAO_AI_QACTAI_H
